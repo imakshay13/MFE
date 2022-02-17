13a3d892e9c26e0924cde25890a3f564be3a579e
Akshay Kumar <imakshay13@gmail.com>
2022-02-17 13:07:16 +0530

    added marketing yml and changes in marketing config

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,7 +1,8 @@
 const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
+
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,8 +1,7 @@
 const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
-const domain = process.env.PRODUCTION_DOMAIN;
+const commonConfig = require("./webpack.common");
 
 const prodConfig = {
   mode: "production",
